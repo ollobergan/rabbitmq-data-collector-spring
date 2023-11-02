28f1eb60f5c2c21f37395eb32c780a982bfbd9ce
Karimov Ollobergan <62586876+ollobergan@users.noreply.github.com>
2023-11-02 18:37:15 +0500

    First commit Project setup

---
+++ api-collector/src/main/java/uz/ollobergan/apicollector/ApiCollectorApplication.java
@@ -0,0 +1,13 @@
+package uz.ollobergan.apicollector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCollectorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiCollectorApplication.class, args);
+    }
+
+}
---
+++ api-collector/src/test/java/uz/ollobergan/apicollector/ApiCollectorApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.ollobergan.apicollector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCollectorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
